cea2b4b09efc508d9c387a22b8080d90fa5074f6
Zach <zachalba@uri.edu>
2024-02-11 19:02:12 -0500

    Slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,20 +1,24 @@
+// Function to display slides in a slideshow automatically
 function showSlides() {
     // Get all elements with the class "mySlides"
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slideshow");
+  
 
-    for (var i = 0; i < slides.length; i++) {
+     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-    }
+     }
      
-    slideindex++;
-    if (slideindex > slides.length) {slideindex = 1};
-    slides[slideindex-1].style.display = "block";
-    
+     slideindex++;
+     if (slideindex > slides.length) {slideindex = 1};
+     slides[slideindex-1].style.display = "block";
+  
+  
+    // Call this function recursively after 5000 milliseconds (5 seconds)
     setTimeout(showSlides, 5000); 
-}
+  }
   
-  
-var slideindex = 0;
-showSlides();
 
-console.log("slideshow.js loaded");
+  
+  var slideindex = 0;
+  showSlides();
+  
